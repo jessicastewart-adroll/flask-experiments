95f04af27b39915e6d9433f666e81654fe7718aa
jessicastewart <jessica.stewart@adroll.com>
2015-04-06 07:13:38 -0700

    finishing data view

--- 6_1_faceboook_ads_app/crm.py
+++ 6_1_faceboook_ads_app/crm.py
@@ -2,8 +2,9 @@
 
 import requests
 import json
+import sqlite3
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -38,26 +39,57 @@ def create_api_request(hash_batch,custom_audience_id):
 @app.route('/', methods=['GET','POST'])
 def upload_file():
 	if request.method == 'POST':
-		hash_file = request.files['file']
-		if hash_file and file_validation(hash_file.filename):
+		uploaded_file = request.files['file']
+		filename = uploaded_file.filename
+		if file_validation(filename): #TODO move validation to js
 			custom_audience_id = request.form['custom_audience_id']
-			raw_data = hash_file.read().split('\r\n')
+			raw_data = uploaded_file.read().split('\r\n')
 			data = cleaner(raw_data)
-			hash_num = str(len(data)) 
+			file_count = str(len(data))
 			batches = batcher(data)	
-			batch_num = str(len(batches))
-			facebook_requests = []
+
+			conn = sqlite3.connect('crm.db')
+			cur = conn.cursor()
+
+			fb_num_received = 0
+			fb_num_invalid_entries = 0
+			fb_invalid_entry_samples = []
+
 			for i, batch in enumerate(batches):
-				print 'Processing batch {}'.format(str(i))
-				facebook_requests.append(create_api_request(batch,custom_audience_id))
-			return '{} hashes in file, {} batches\n\n\n'.format(hash_num, batch_num) + str(facebook_requests)
+				print 'Processing batch {}'.format(str(i)) #TODO return job progress to browser via ajax 
+				facebook_response = json.loads(create_api_request(batch,custom_audience_id))
+				fb_num_received += facebook_response['num_received']
+				fb_num_invalid_entries += facebook_response['num_invalid_entries']
+				if facebook_response['invalid_entry_samples']:
+					fb_invalid_entry_samples.append(facebook_response['invalid_entry_samples'])
+
+			values = 'values({}, "{}", {}, {}, {}, "{}")'.format(custom_audience_id, filename, file_count, fb_num_received, fb_num_invalid_entries, fb_invalid_entry_samples)
+			insert = '''INSERT INTO crm_uploads({}) {}'''.format('custom_audience_id, filename, file_count, fb_num_received, fb_num_invalid_entries, fb_invalid_entry_samples', values)
+			cur.execute(insert)
+			conn.commit()
+			conn.close()
+			return redirect(url_for('get_custom_audience_data', custom_audience_id=custom_audience_id))
 	return render_template('upload.html')
 
 @app.route('/<custom_audience_id>')
 def get_custom_audience_data(custom_audience_id):
 	url = 'https://graph.facebook.com/v2.3/{}?access_token={}&fields=id,account_id,name,approximate_count,data_source,delivery_status,operation_status,subtype'.format(custom_audience_id,token)
-	r = requests.get(url)
-	return str(r.text)
+	select = 'SELECT * FROM crm_uploads WHERE custom_audience_id={} ORDER BY upload_timestamp DESC'.format(custom_audience_id)
+
+	conn = sqlite3.connect('crm.db')
+	cur = conn.cursor()
+	cur.execute(select)  #returning 'OperationalError: no such column: favicon.ico' but not affecting functionality, likely due to sqlite3 idiosyncracy 
+	db_data = cur.fetchall()
+	conn.close()
+	if not db_data:
+		db_data = 'No data uploaded to this custom audience via this tool'
+
+	fb_data = json.loads(requests.get(url).text)
+
+	return render_template('return_data.html',
+							custom_audience_id=custom_audience_id,
+							fb_data=fb_data,
+							db_data=db_data)
 
 if __name__ == '__main__':
 	app.run(debug=True)
